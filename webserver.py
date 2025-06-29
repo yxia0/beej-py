91e1909d1332def740cb7c24e5131d0a5282b2d1
Youning Xia <youning.xia@physicsx.ai>
2025-06-29 22:12:57 +0100

    Add wip

--- webserver.py
+++ webserver.py
@@ -1,8 +1,22 @@
+import os.path
 import socket
+from dataclasses import dataclass
+
+@dataclass
+class HTTPRequestHeader:
+    request_type: str
+    request_target: str
+    host: str
 
 
-if __name__ == "__main__":
 
+def parse_request_header(raw_header: str) -> HTTPRequestHeader:
+    header_by_line = raw_header.split("\r\n")
+    request_type, request_target, http_version = header_by_line[0].split(" ")
+    host = header_by_line[1].split("Host: ")[1]
+    return HTTPRequestHeader(request_type, request_target, host)
+
+def run_server():
     listen_sock = socket.socket()
 
     # Magic line:
@@ -16,21 +30,44 @@ if __name__ == "__main__":
         new_conn = listen_sock.accept()
         new_socket = new_conn[0]
 
-
-        full_request = ""
+        full_request_header = ""
         while True:
-            # recv will return empty string only when the client closes its
+            # recv will return an empty string only when the client closes its
             # connection; but the client will be waiting for the response, so
             # it will not close the connection!! so empty string never gets returned
             # unless the client crashes or something.
             request_bytes = new_socket.recv(1024)
             request_str = request_bytes.decode("ISO-8859-1")
-            full_request += request_str
 
             if "\r\n\r\n" in request_str:
+                remaining_header, request_body = request_str.split("\r\n\r\n")
+                full_request_header += remaining_header
                 break
+            else:
+                full_request_header += request_str
 
-        print(f"Received request: \n{full_request}\n")
+        header_object = parse_request_header(full_request_header)
+
+        file_path = os.path.normpath(os.path.join("data", header_object.request_target))
+        file_type = os.path.splitext(file_path)[1]
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+
 
         response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 6\r\nConnection: close\r\n\r\nHello!"
         response_bytes = response.encode("ISO-8859-1")
